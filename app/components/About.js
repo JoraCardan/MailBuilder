8baee832496b9f96116beb6205312a736587d9cd
Constantin Melniciuc <c.melniciuc@youngcapital.nl>
2017-01-09 11:33:15 +0100

    Updated About

--- app/components/About.js
+++ app/components/About.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
 const About = () =>
+  <div>
+    Hey! Thanks for using this example. If you like it, consider starring the repo :))
     <div>
-        Hey! Thanks for using this example. If you like it, consider starring the repo :))
-        <div>
-            <iframe
-                src="https://ghbtns.com/github-btn.html?user=jpsierens&repo=webpack-react-redux&type=star&size=large"
-                frameBorder="0"
-                allowTransparency="true"
-                scrolling="0">
-            </iframe>
-        </div>
-    </div>;
+      <a href="https://github.com/JoraCardan/MailBuilder" target="_blank">Mail Builder Source</a>
+    </div>
+  </div>;
 
 
 export default About;
